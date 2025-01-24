6704ae73fb4a749509b422d6c5e04102acdb0a7b
Symond1 <maheshwarisymond@gmail.com>
2025-01-24 13:03:16 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+var config = {
+    lines: [
+      "  The first time I saw you in class, my heart skipped a beat, and your smile was breathtaking. ❤️🌹",
+        "I wanted to tell you how I felt on that same day, but I could not gather the courage at that very moment. 💔",
+        "When I finally spoke my heart to you, my world changed in the most beautiful way. 💞",
+        "You brought light and color into my life, making it bloom like never before. 🌸❤️",
+        "I know I have made mistakes and hurt you more than I ever wanted, but you are my everything. 😔💔❤️‍🩹",
+        "With you by my side, I feel I have everything I could ever need, and I am sorry for the pain caused. ❤️🌹",
+        "You are the only one I love, the only one I will ever need, and my heart will always belong.",
+
+        "Your silly cupid"
+    ],
+
+}
